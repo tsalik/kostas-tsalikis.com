548b882e0c8301166bb67fcde291811f7bc0beed
Kostas Tsalikis <tsalikispk@gmail.com>
2025-04-04 23:25:16 +0300

    Fix checking date of posts

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:1313',
-    supportFile: false,
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  },
-}) 
